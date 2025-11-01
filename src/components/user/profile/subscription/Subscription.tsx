d7b147e0c3c49d53522304912734263f31edb5af
socday <socpro13579@gmail.com>
2025-11-01 17:16:51 +0700

    Feat: add subscription package mobile

--- src/components/user/profile/subscription/Subscription.tsx
+++ src/components/user/profile/subscription/Subscription.tsx
@@ -1,20 +1,34 @@
-import React from 'react';
+import React, { use } from 'react';
 import "./styles/Subscription.css";
+import { useMediaQuery } from 'react-responsive';
+import { is } from '@react-three/fiber/dist/declarations/src/core/utils';
 
 
 export default function Subscription() {
+
+    const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
+    const isDesktop = useMediaQuery({ query: "(min-width: 769px)" });
     return (
         <div className="orders">
             <div className="profile-subscription">
+                {isDesktop &&
+                <>
                 <div className = "profile-subscription-left">
                     <div className= "ps-image">
-
                     </div>
-                    <input type="text" placeholder="Gói mua tuần"  />                    
-
-
+                    <input type="text" placeholder="Gói mua tuần"   />                    
                 </div>
+                </>}
                 <div className = "profile-subscription-right">
+
+
+                {isMobile && <>
+
+                    <span className='p-w-package'>Gói mua tuần</span>
+                    <div className= "ps-image">
+                    </div>
+                    </>}
+                    
                     <div className="form-fields">
                             <input type="text" placeholder="Ngày bắt đầu: dd/mm/yy"  />
                             <input type="text" placeholder="Ngày kết thúc: dd/mm/yy"  />
